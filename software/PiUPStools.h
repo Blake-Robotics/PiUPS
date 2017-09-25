27647aca37a1e472ee9d3feb5ebb2b235fbd9e0f
Edward Overton <edward.overton@gmail.com>
2017-09-25 20:34:18 +0000

    Adding doxygen documentation

--- software/PiUPStools.h
+++ software/PiUPStools.h
@@ -1,11 +1,19 @@
 #ifndef PIUPSTOOLS_H
 #define PIUPSTOOLS_H
+/** \file
+ *  \brief  PiUPS Utility functions
+ *  A set of utility functions for accessing the PiUPS
+ *  registers over the I2C bus.
+ */
 
 
 #include "i2ctools.h"
 
 
 /// Print the current status of the ups system:
+/* Function to print the current status of the ups system to screen
+ * @param file I2C file connected to the PiUPS device.
+ */
 void piups_printstats(int file);
 
 
